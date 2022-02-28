17f198d32020c88fabe2dc6cdd4395a91be137ed
chaulotj <chaulotj@gmail.com>
2022-02-27 22:50:08 -0500

    asteroids

---
+++ Game4Group4/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(transform.position.x) > 15f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Game4Group4/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{
+    public GameObject asteroid;
+    public GameObject rocket;
+    [SerializeField] private float randomSpawnMin;
+    [SerializeField] private float randomSpawnMax;
+    private float timer;
+    [SerializeField] private float xRight;
+    [SerializeField] private float xLeft;
+    [SerializeField] private float yDiff;
+    [SerializeField] private float randomSpeedMin;
+    [SerializeField] private float randomSpeedMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0f)
+        {
+            timer = Random.Range(randomSpawnMin, randomSpawnMax);
+            float xDir = -1f;
+            float x = xRight;
+            if(Random.value < .5f)
+            {
+                x = xLeft;
+                xDir = 1f;
+            }
+            GameObject newAsteroid = Instantiate(asteroid, new Vector2(x, rocket.transform.position.y + yDiff), Quaternion.identity, transform);
+            newAsteroid.GetComponent<Rigidbody>().velocity = new Vector2(xDir, Random.Range(-1f, 1f)) * Random.Range(randomSpeedMin, randomSpeedMax);
+        }
+    }
+}
